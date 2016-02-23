df0002af6a35369dc6eae0618f8806a89816f576
harry <harry@harry-Satellite-C655D>
2016-02-23 16:13:42 -0500

    added javafx coding

--- checkedmemes/src/checkedmemes/Checkedmemes.java
+++ checkedmemes/src/checkedmemes/Checkedmemes.java
@@ -1,8 +1,20 @@
 package checkedmemes;
 
-import java.util.Scanner;
-public class Checkedmemes {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
+import java.util.Scanner;
+public class Checkedmemes extends Application{
+
+public void start(Stage primarystage){
+    
+}    
+    
 public static void main(String[] args) {
 Scanner stdin = new Scanner(System.in);
 
